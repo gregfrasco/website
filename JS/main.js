59fd5ab241c0ac7274747ecbe60cb9dc5db615c8
Greg Frasco <frascog@wit.edu>
2016-02-12 13:37:26 -0500

    Updated Photography Page

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,7 @@ var feed = new Instafeed({
 		userId: 1400590791,
 		accessToken: '94764.1677ed0.c6256a27eddf41709ddf29af3469a4e5',
 		target: 'instagram',
-		limit: '30',
+		limit: '24',
 		sortBy: 'most-liked',
 		resolution: 'standard_resolution',
 		template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes"><p>&hearts; {{likes}}</p></div>',
