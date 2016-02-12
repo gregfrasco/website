c8f1a765d727df559140f8624f11289929b1fc18
Greg Frasco <frascog@wit.edu>
2016-02-12 14:09:46 -0500

    Added Location to Photography

--- JS/main.js
+++ JS/main.js
@@ -3,10 +3,10 @@ var feed = new Instafeed({
 		userId: 1400590791,
 		accessToken: '94764.1677ed0.c6256a27eddf41709ddf29af3469a4e5',
 		target: 'instagram',
-		limit: '24',
+		limit: '48',
 		sortBy: 'most-liked',
 		resolution: 'standard_resolution',
-		template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes"><p><i class="large red heart icon"></i> {{likes}}</p></div>',
+		template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes"><p><i class="large red heart icon"></i> {{likes}}<br>{{location}}</p></div>',
 });
 
 window.onload = function() {
